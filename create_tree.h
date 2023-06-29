6e118708ab4f24a10fa449f6c0317d6dbf2d955e
Gabriel Pereira <gabrielpereirajp1201@gmail.com>
2023-06-29 08:09:57 -0300

    Transforme arquivo .cpp em .h

--- create_tree.h
+++ create_tree.h
@@ -10,6 +10,10 @@ struct Node
     struct Node* ptrRight;
 };
 
+////////////////////////////////////////////////////////////////////////////////
+/*                         Função que cria um novo nó                         */
+////////////////////////////////////////////////////////////////////////////////
+
 // Cria um novo nó
 struct Node* newNode(int iData)
 {
@@ -20,17 +24,25 @@ struct Node* newNode(int iData)
     return newNode;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/*                  Função que insere um novo nó na árovre                    */
+////////////////////////////////////////////////////////////////////////////////
+
 // Insere um novo nó na árvore
 struct Node* insertNode(struct Node* node, int iData)
 {
     if (node == nullptr) return newNode(iData);
-    // Sempre que o valor a ser inserido for menor que o valor do nó atual, o valor
-    // será inserido à esquerda por recursão
+    // Sempre que o valor a ser inserido for menor que o valor do nó atual, o 
+    // valor será inserido à esquerda por recursão
     else if (iData < node->iPayload) node->ptrLeft = insertNode(node->ptrLeft, iData);
     // Caso contrário, o valor será inserido à direita por recursão
     else node->ptrRight = insertNode(node->ptrRight, iData);
     return node;
 }
+
+////////////////////////////////////////////////////////////////////////////////
+/*                  Funções que percorrem a árvore binária                    */
+////////////////////////////////////////////////////////////////////////////////
 
 // Percorre a árvore com pré-ordem
 void traversePreOrder(struct Node* ptrStartingNode)
@@ -46,6 +58,8 @@ void traversePreOrder(struct Node* ptrStartingNode)
     }
 }
 
+/*----------------------------------------------------------------------------*/
+
 // Percorre a árvore em ordem
 void traverseInOrder(struct Node* ptrStartingNode)
 {
@@ -60,6 +74,8 @@ void traverseInOrder(struct Node* ptrStartingNode)
     }
 }
 
+/*----------------------------------------------------------------------------*/
+
 // Percorre a árvore com pós-ordem
 void traversePostOrder(struct Node* ptrStartingNode)
 {
@@ -73,6 +89,10 @@ void traversePostOrder(struct Node* ptrStartingNode)
         cout << " " << ptrStartingNode->iPayload;
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////
+/*                   Função que deleta um nó da árvore                        */
+////////////////////////////////////////////////////////////////////////////////
 
 // Deleta um nó da árvore
 struct Node* deleteNode(struct Node* node, int iData)
@@ -92,25 +112,25 @@ struct Node* deleteNode(struct Node* node, int iData)
             free(node);
             node = nullptr;
         }
-        // Se o nó tiver apenas um filho (à direita), o nó será deletado e o filho
-        // ocupará seu lugar
+        // Se o nó tiver apenas um filho (à direita), o nó será deletado e o 
+        // filho ocupará seu lugar
         else if (node->ptrLeft == nullptr)
         {
             struct Node* ptrTemp = node;
             node = node->ptrRight;
             free(ptrTemp);
         }
-        // Se o nó tiver apenas um filho (à esquerda), o nó será deletado e o filho
-        // ocupará seu lugar
+        // Se o nó tiver apenas um filho (à esquerda), o nó será deletado e o 
+        // filho ocupará seu lugar
         else if (node->ptrRight == nullptr)
         {
             struct Node* ptrTemp = node;
             node = node->ptrLeft;
             free(ptrTemp);
         }
-        // Se o nó tiver dois filhos, o nó será deletado e o menor valor da sub-árvore
-        // à direita ocupará seu lugar (o menor valor da sub-árvore à direita é o
-        // valor mais à esquerda da sub-árvore à direita)
+        // Se o nó tiver dois filhos, o nó será deletado e o menor valor da 
+        // sub-árvore à direita ocupará seu lugar (o menor valor da sub-árvore à 
+        // direita é o valor mais à esquerda da sub-árvore à direita)
         else
         {
             struct Node* temp = node->ptrRight;
@@ -122,19 +142,27 @@ struct Node* deleteNode(struct Node* node, int iData)
     return node;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/*                  Função que busca um nó na árvore binária                  */
+////////////////////////////////////////////////////////////////////////////////
+
 // Busca o endereço de memória de um nó na árvore
 struct Node* searchNode(struct Node* node, int iData)
 {
     if (node == nullptr) return nullptr;
-    // Se o valor a ser buscado for igual ao valor do nó atual, o endereço de memória
-    // do nó atual será retornado
+    // Se o valor a ser buscado for igual ao valor do nó atual, o endereço de 
+    // memória do nó atual será retornado
     else if (iData == node->iPayload) return node;
-    // Se o valor a ser buscado for menor que o valor do nó atual, a busca será feita
-    // à esquerda por recursão
+    // Se o valor a ser buscado for menor que o valor do nó atual, a busca será 
+    // feita à esquerda por recursão
     else if (iData < node->iPayload) return searchNode(node->ptrLeft, iData);
     // Caso contrário, a busca será feita à direita por recursão
     else return searchNode(node->ptrRight, iData);
 }
+
+////////////////////////////////////////////////////////////////////////////////
+/*   Função que constrói uma árvore binária a partir de um arquivo de texto   */
+////////////////////////////////////////////////////////////////////////////////
 
 // Constrói uma árvore binária de busca a partir de um arquivo texto
 struct Node* constructTreeFromFile(const string& strFileName)
@@ -159,6 +187,10 @@ struct Node* constructTreeFromFile(const string& strFileName)
     return ptrRoot;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/*   Função que constrói uma árvore binária com dados inseridos pelo usuário  */
+////////////////////////////////////////////////////////////////////////////////
+
 // Constrói uma árvore binária de busca a partir de dados inseridos pelo usuário
 struct Node* constructTreeFromUserInput()
 {
@@ -169,6 +201,9 @@ struct Node* constructTreeFromUserInput()
     {
         cout << "Digite um valor inteiro (0 para sair): ";
         cin >> iData;
+        // Se o valor inserido for 0, o programa pergunta se o usuário realmente 
+        // deseja sair. Se a resposta for sim, o programa é encerrado. Caso 
+        // contrário, o programa volta a pedir um valor
         if (iData == 0)
         {
             cout << "Você realmente deseja sair? (S/s): ";
@@ -176,6 +211,7 @@ struct Node* constructTreeFromUserInput()
             cin >> cAnswer;
             if (cAnswer == 'S' || cAnswer == 's') break;
         }
+        // Se o usuário não quiser sair, o 0 será inserido na árvore
         ptrRoot = insertNode(ptrRoot, iData);
     }
     return ptrRoot;
