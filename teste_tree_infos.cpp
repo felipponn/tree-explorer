56409dc7fd208f21f6dfb9f5847bb6db936776d3
atronee <otavio.oam@protonmail.com>
2023-06-30 08:47:27 -0300

    atualize o arquivo de testes de tree_infos

--- teste_tree_infos.cpp
+++ teste_tree_infos.cpp
@@ -4,29 +4,85 @@
 
 using namespace std;
 
+// ========================================================================= //
+// -------------------------------  TESTE ----------------------------------
+// Arquivo para testar as funções de informações de árvores
+// As principais funções da tree_infos.h são:
+// - height: calcula a altura de uma árvore
+// - size: calcula o tamanho de uma árvore
+// - full: verifica se uma árvore é cheia
+// - perfect: verifica se uma árvore é perfeita
+// - complete: verifica se uma árvore é completa
+// ========================================================================= //
+
+
 int main()
 {
-    struct Node* root = new Node;
-    root->iPayload = 10;
-    root->ptrLeft = nullptr;
-    root->ptrRight = nullptr;
+    cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl;
+    cout << "Testando a funcao que calcula a altura de uma arvore: " << endl;
+    cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl;
 
-    insertNode(root, 5);
-    insertNode(root, 15);
-    insertNode(root, 3);
-    insertNode(root, 7);
-    insertNode(root, 12);
-    insertNode(root, 17);
-    insertNode(root, 1);
-    insertNode(root, 4);
-    insertNode(root, 6);
-    insertNode(root, 8);
-    insertNode(root, 11);
-    insertNode(root, 13);
+    struct Node* ptrRoot = nullptr;
+    ptrRoot = insertNode(ptrRoot, 10);
+    ptrRoot = insertNode(ptrRoot, 5);
+    ptrRoot = insertNode(ptrRoot, 15);
+    ptrRoot = insertNode(ptrRoot, 3);
+    ptrRoot = insertNode(ptrRoot, 7);
+    ptrRoot = insertNode(ptrRoot, 13);
+    ptrRoot = insertNode(ptrRoot, 17);
+    ptrRoot = insertNode(ptrRoot, 2);
+    ptrRoot = insertNode(ptrRoot, 4);
+    ptrRoot = insertNode(ptrRoot, 6);
+    ptrRoot = insertNode(ptrRoot, 8);
+    ptrRoot = insertNode(ptrRoot, 12);
+    ptrRoot = insertNode(ptrRoot, 14);
+    ptrRoot = insertNode(ptrRoot, 16);
+    ptrRoot = insertNode(ptrRoot, 18);
 
-    cout << "é completa? " << complete(root) << endl; // sim
-    cout << "é perfeita? " << perfect(root) << endl; // não
-    cout << "altura: " << height(root) << endl; // 4
-    cout << "tamanho: " << size(root) << endl; // 13
+    cout << "Arvore: ";
+    traversePreOrder(ptrRoot);
+    cout << endl;
+
+    cout << "Altura da arvore: " << height(ptrRoot) << endl;
+
+    cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl;
+    cout << "Testando a funcao que calcula o tamanho de uma arvore: " << endl;
+    cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl;
+
+    cout << "Tamanho da arvore: " << size(ptrRoot) << endl;
+
+    cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl;
+    cout << "Testando a funcao que verifica se uma arvore eh cheia: " << endl;
+    cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl;
+
+    cout << "Arvore: ";
+    traversePreOrder(ptrRoot);
+    cout << endl;
+
+    if (full(ptrRoot)) cout << "A arvore eh cheia" << endl;
+    else cout << "A arvore nao eh cheia" << endl;
+
+    cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl;
+    cout << "Testando a funcao que verifica se uma arvore eh perfeita: " << endl;
+    cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl;
+
+    cout << "Arvore: ";
+    traversePreOrder(ptrRoot);
+    cout << endl;
+
+    if (perfect(ptrRoot)) cout << "A arvore eh perfeita" << endl;
+    else cout << "A arvore nao eh perfeita" << endl;
+
+    cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl;
+    cout << "Testando a funcao que verifica se uma arvore eh completa: " << endl;
+    cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl;
+
+    cout << "Arvore: ";
+    traversePreOrder(ptrRoot);
+    cout << endl;
+
+    if (complete(ptrRoot)) cout << "A arvore eh completa" << endl;
+    else cout << "A arvore nao eh completa" << endl;
+
     return 0;
 }
